392a00adab642f8b65f8cb40a40c043bddde6046
cchens <mail@christinachen.ca>
2015-05-28 13:51:45 -0400

    Show RNA

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -7,6 +7,7 @@ converter.controller('bodyCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.convertDNA = function() {
         $http.post('http://localhost:9000/convertDNA', $scope.entry).success(function (data) {
             $scope.currentAA = data.aa;
+            $scope.currentRNA = data.rna;
             $scope.dnadone = true;
         });
     };
